fbcc3d313226dbc255a0ef66a048c33e7e45e75b
brooke@anvil.works <brooke@anvil.works>
2025-03-19 15:36:54 +0000

    Edited _Components.Avatar

--- client_code/_Components/Avatar/__init__.py
+++ client_code/_Components/Avatar/__init__.py
@@ -36,6 +36,7 @@ class Avatar(AvatarTemplate):
   def fallback_icon(self, value) -> str:
     """The icon to display on this component."""
     fallback_icon = self.dom_nodes['anvil-m3-avatar-icon']
+    fallback_icon.style.display = "none"
     if value:
       fallback_icon.className = ""
       fallback_icon.classList.add("material-symbols-outlined")
@@ -49,4 +50,10 @@ class Avatar(AvatarTemplate):
     image = self.dom_nodes['anvil-m3-avatar-image']
     print(value)
     image.src = value
+
+  @anvil_prop
+  @property
+  def name(self, value):
+    initials_div = self.dom_nodes['anvil-m3-avatar-initials']
+    name =
     
