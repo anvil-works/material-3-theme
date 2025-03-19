8188ba25f3163eb258e2b33042bc62a6913f9ecb
brooke@anvil.works <brooke@anvil.works>
2025-03-19 14:11:42 +0000

    Edited _Components.NavigationLink and _Components.Avatar

--- client_code/_Components/Avatar/__init__.py
+++ client_code/_Components/Avatar/__init__.py
@@ -5,6 +5,13 @@ from anvil import *
 class Avatar(AvatarTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    self.tag = ComponentTag()
+    self._props = properties
+    self._tooltip_node = None
     self.init_components(**properties)
+
+    self.dom_nodes['anvil-m3-avatar'].addEventListener(
+      "click", self._handle_click
+    )
 
     # Any code you write here will run before the form opens.
