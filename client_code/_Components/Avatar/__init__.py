f4acb347824ba0583d63dcf2c0de9e15fea24379
brooke@anvil.works <brooke@anvil.works>
2025-03-19 14:26:53 +0000

    Edited _Components.Avatar

--- client_code/_Components/Avatar/__init__.py
+++ client_code/_Components/Avatar/__init__.py
@@ -14,4 +14,26 @@ class Avatar(AvatarTemplate):
       "click", self._handle_click
     )
 
+    
+  def _handle_click(self, event):
+    self.raise_event(
+      "click",
+      event=event,
+      keys={
+        "shift": event.shiftKey,
+        "alt": event.altKey,
+        "ctrl": event.ctrlKey,
+        "meta": event.metaKey,
+      },
+    )
     # Any code you write here will run before the form opens.
+
+  @anvil_prop
+  @property
+  def icon(self, value) -> str:
+    """The icon to display on this component."""
+    link_icon = self.dom_nodes['anvil-m3-navigation-link-icon']
+    if value:
+      link_icon.className = ""
+      link_icon.classList.add("material-symbols-outlined")
+      link_icon.innerText = value[3:]
