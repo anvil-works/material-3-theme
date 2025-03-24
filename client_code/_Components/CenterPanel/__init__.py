da62ba18cb701711a70164af87c21229a451f3ab
patricia@anvil.works <patricia@anvil.works>
2025-03-24 14:38:58 +0000

    Edited _Components.CenterPanel

--- client_code/_Components/CenterPanel/__init__.py
+++ client_code/_Components/CenterPanel/__init__.py
@@ -1,9 +1,10 @@
-from ._anvil_designer import CenterPanelTemplate
 from anvil import *
 
 from ..._utils.properties import (
+  get_unset_spacing,
   spacing_property,
 )
+from ._anvil_designer import CenterPanelTemplate
 
 
 class CenterPanel(CenterPanelTemplate):
@@ -11,4 +12,9 @@ class CenterPanel(CenterPanelTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    def _anvil_get_unset_property_values_(self):
+      el = self.dom_nodes["anvil-m3-center-panel"]
+      sp = get_unset_spacing(el, el, self.spacing)
+      return {"spacing": sp}
+
+    spacing = spacing_property('anvil-m3-center-panel')
