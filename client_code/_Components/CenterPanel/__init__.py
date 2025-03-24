3c8015fe3ccac0bc5a41adb9b0e8ce2aca0989f6
patricia@anvil.works <patricia@anvil.works>
2025-03-24 15:48:21 +0000

    Edited _Components.CenterPanel

--- client_code/_Components/CenterPanel/__init__.py
+++ client_code/_Components/CenterPanel/__init__.py
@@ -3,20 +3,25 @@ import anvil.designer
 
 from ..._utils.properties import (
   get_unset_spacing,
+  role_property,
   spacing_property,
+  tooltip_property,
 )
 from ._anvil_designer import CenterPanelTemplate
 
 
 class CenterPanel(CenterPanelTemplate):
-    def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
-        self._props = properties
-        self.init_components(**properties)
-    
-        def _anvil_get_unset_property_values_(self):
-            el = self.dom_nodes["anvil-m3-center-panel"]
-            sp = get_unset_spacing(el, el, self.spacing)
-            return {"spacing": sp}
-    
-    spacing = spacing_property('anvil-m3-center-panel')
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self._props = properties
+    self._tooltip_node = None
+    self.init_components(**properties)
+
+    def _anvil_get_unset_property_values_(self):
+      el = self.dom_nodes["anvil-m3-center-panel"]
+      sp = get_unset_spacing(el, el, self.spacing)
+      return {"spacing": sp}
+
+  spacing = spacing_property('anvil-m3-center-panel')
+  role = role_property('anvil-m3-center-panel')
+  tooltip = tooltip_property('anvil-m3-text-container')
